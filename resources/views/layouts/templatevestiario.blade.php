f0970b7837a334a49fe8059a614fad0331476c79
Tr3Spark <s_park_91@hotmail.it>
2022-05-02 10:59:57 +0200

    Prova con antonio

--- resources/views/layouts/templatevestiario.blade.php
+++ resources/views/layouts/templatevestiario.blade.php
@@ -22,6 +22,7 @@
                                     viewBox="0 0 24 24"
                                     stroke="currentColor"
                                 >
+                                
                                     <path d="M4 6h16M4 12h16M4 18h16"></path>
                                 </svg>
                             </button>
